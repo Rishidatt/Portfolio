22bafb175bae43973685e56ccecc39a6019363bf
Rishi <rdatt46@gmail.com>
2024-10-30 00:53:24 +0530

    Updated index.html, script.js, style.css, and added .vscode

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Fitness Coach", "Backend-Developer", "Designer", "Freelancer"],
+        strings: ["", "Fitness Coach", "SEO Expert", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Fitness Coach", "Backend-Developer", "Designer", "Freelancer"],
+        strings: ["", "Fitness Coach", "SEO Expert", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
